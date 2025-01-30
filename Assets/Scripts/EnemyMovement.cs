1ada18c0b6ec8fa3baa4e7d45503e7a6a2da46be
sparKlZJuNiO <jrrossjob@gmail.com>
2025-01-30 01:19:38 +0000

    kik

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -9,11 +9,14 @@ public class EnemyMovement : MonoBehaviour
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
     public bool grounded = false;
+    public bool walking = false;
+    public bool shooting = false;
     [SerializeField] private SpriteRenderer enemy;
     private Vector2 UpdatePosition;
-    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private GameObject[] waypoints;
     public bool Ready = false;
     public bool waypointing = true;
+    Animator anim;
     public int waypointIndex = 0;
 
 
@@ -23,29 +26,40 @@ public class EnemyMovement : MonoBehaviour
         rb = this.GetComponent<Rigidbody2D>();
         enemy = GetComponent<SpriteRenderer>();
         transform.position = waypoints[waypointIndex].transform.position;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float distance = Vector2.Distance(transform.position, player.transform.position);
-        UpdatePosition = new Vector2(transform.position.x, transform.position.y); // Keeps the y-axis height
-        //Debug.Log(distance);
-
-
+                                                                     
+  
         if (distance < 16 && rb.GetComponent<Health>().death == false)
         {
-            Vector3 direction = player.position - transform.position;
+            Vector3 direction = player.transform.position - transform.position;
             direction.Normalize();
             movement = direction;
             enemy.flipX = player.transform.position.x < this.transform.position.x; // Flips enemy
             Ready = true;
             waypointing = false;
+            Debug.Log("Close");
+            walking = false;
+            rb.GetComponent<EnemyShooting>().shooting = true;
+            anim.SetBool("shooting", shooting);
+            anim.SetBool("walking", walking);
         }
         else
         {
             Ready = false;
             waypointing = true;
+            walking = true;
+            rb.GetComponent<EnemyShooting>().shooting = false;
+            anim.SetBool("shooting", shooting);
+            anim.SetBool("walking", walking);
+            Debug.Log("Distance");
+            waypoints[0].GetComponent<BoxCollider2D>().isTrigger = false;
+            waypoints[1].GetComponent<BoxCollider2D>().isTrigger = false;
         }
 
        if (rb.GetComponent<Health>().death == false && Ready == true && waypointing == false)
@@ -66,7 +80,10 @@ public class EnemyMovement : MonoBehaviour
     {
         if (waypointIndex <= waypoints.Length - 1 && waypointing == true && Ready == false)
         {
-            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+            waypoints[0].GetComponent<BoxCollider2D>().isTrigger = false;
+            waypoints[1].GetComponent<BoxCollider2D>().isTrigger = false;
+            moveSpeed = 5f;
         }
     }
 
@@ -75,6 +92,7 @@ public class EnemyMovement : MonoBehaviour
         if (collision.gameObject.tag == ("grounded"))
         {
             grounded = false;
+            anim.SetBool("grounded", grounded);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -87,19 +105,19 @@ public class EnemyMovement : MonoBehaviour
         if (collision.gameObject.tag == ("grounded"))
         {
             grounded = true;
+            anim.SetBool("grounded", grounded);
         }
 
         if (collision.gameObject.tag == ("waypoint") && waypointing == true)
         {
-                waypointIndex += 1;
+                waypointIndex = 1;
                 enemy.flipX = false;
-            collision.gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
         }
+       
         if (collision.gameObject.tag == ("waypoint2") && waypointing == true)
         {
             enemy.flipX = true;
-            waypointIndex -= 1;
-            collision.gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+            waypointIndex = 0;
         }
     }
 
@@ -109,9 +127,10 @@ public class EnemyMovement : MonoBehaviour
         if (Ready == true && waypointing == false)
         {
             rb.MovePosition((Vector2) transform.position + (direction * moveSpeed * Time.deltaTime));
-            transform.position = new Vector2(transform.position.x, transform.position.y); // This would stick the player down the y-axis
-            waypointIndex = 0;
-            moveSpeed = 5f;
+            waypointIndex = 1;
+            waypoints[0].GetComponent<BoxCollider2D>().isTrigger = true;
+            waypoints[1].GetComponent<BoxCollider2D>().isTrigger = true;
+            moveSpeed = 55f;
         }
     }
 }
